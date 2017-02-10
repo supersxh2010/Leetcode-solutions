452aa06eb6944e1111c329894146fa66c2bc6208
Zach Sui <supersxh2010@hotmail.com>
2017-02-10 17:35:40 -0500

    Add files via upload

---
+++ LeetCode/src/com/easy/leetcode/Valid_Anagram_242.java
@@ -0,0 +1,14 @@
+package com.easy.leetcode;
+
+public class Valid_Anagram_242 {
+	
+	public boolean isValidAnagram(String s1, String s2){
+		if(s1.length()!=s2.length()){
+			return false;
+		}
+		
+		
+		return true;
+	}
+
+}
